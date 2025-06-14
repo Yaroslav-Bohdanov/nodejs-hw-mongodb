73ae123f9f267b07de040058cfff4c30de6430c2
Ярослав Богданов <bogdanov.yar99@gmail.com>
2025-06-15 00:19:44 +0300

    fix http-errors

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,17 @@
+import { HttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
-  const statusCode = err.status || 500;
-  res.status(statusCode).json({
-    status: statusCode,
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: err,
+    });
+    return;
+  }
+
+  res.status(500).json({
+    status: 500,
     message: 'Something went wrong',
     data: err.message,
   });
