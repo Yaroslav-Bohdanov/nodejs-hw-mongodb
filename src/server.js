0668756741265acd913b9fd47ae7e82802cd2dbc
Ярослав Богданов <bogdanov.yar99@gmail.com>
2025-07-13 00:29:32 +0300

    init hw7-swagger

--- src/server.js
+++ src/server.js
@@ -7,8 +7,12 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import swaggerUI from 'swagger-ui-express';
+import path from 'node:path';
+import fs from 'node:fs';
 
 const port = Number(getEnvVar('PORT'));
+const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
 
 export const setupServer = () => {
   const app = express();
@@ -23,6 +27,16 @@ export const setupServer = () => {
     }),
   );
   app.use('/uploads', express.static(UPLOAD_DIR));
+
+  try {
+    const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
+    app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+  } catch {
+    app.use('/api-docs', (req, res) =>
+      res.status(500).json({ message: "Can't load swagger docs" }),
+    );
+  }
+
   app.use(router);
   app.use(notFoundHandler);
   app.use(errorHandler);
